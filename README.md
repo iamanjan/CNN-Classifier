ef9e5e3f4dbdc829dd3ed9c5e5fc9fbd71e1f5fd
iamanjan~ <anji1203@gmail.com>
2023-09-30 22:31:38 +0530

    updated template

--- README.md
+++ README.md
@@ -13,4 +13,27 @@ $ touch README.md
 add files to git through source control at vs code
 
 files will be created at git,then create .gitgnore with template as python , and LICENSE with mit license file at git branch by adding new files.
-
+```
+to pull all files from git
+$ git pull origin master
+it will add files to vscode
+```
+create template.py file by add new file 
+```
+to check the path of the python file
+use python terminal
+```
+$ python
+>>> from pathlib import Path
+>>> Path('x/y/z.txt')
+it shows windows path like WindowsPath('x/y/z.txt')
+```
+to split the path and file at python terminal
+```
+>>> import os
+>>> os.path.split("x/y/z.txt")
+('x/y', 'z.txt')
+>>> os.path.split("z.txt")
+('', 'z.txt')
+>>>
+```
--- template.py
+++ template.py
@@ -1,5 +1,5 @@
 import os
-from pathlib import Path
+from pathlib import Path #windows path for folder/file
 import logging
 
 
@@ -7,9 +7,9 @@ logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s")
 
 package_name= "CNNClassifier"
 
-
+# write code for create folder structure
 list_of_files=[
-   ".github/workflows/.gitkeep",
+   ".github/workflows/.gitkeep", # github is a folder name
    f"src/{package_name}/__init__.py", 
    f"src/{package_name}/components/__init__.py", 
    f"src/{package_name}/utils/__init__.py", 
@@ -17,26 +17,26 @@ list_of_files=[
    f"src/{package_name}/pipeline/__init__.py", 
    f"src/{package_name}/entity/__init__.py", 
    f"src/{package_name}/constants/__init__.py",
-   "tests/__init__.py",
+   "tests/__init__.py", # tests is folder name
    "tests/unit/__init__.py",
    "tests/integration/__init__.py",
    "configs/config.yaml",
    "params.yaml",
    "init_setup.sh",
    "requirements.txt", 
-   "requirements_dev.txt",
+   "requirements_dev.txt", # its development envronment
    "setup.py",
    "setup.cfg",
    "pyproject.toml",
    "tox.ini",
    "research/trials.ipynb", 
                 ]
-
+# write code for create all above folder 
 for filepath in list_of_files:
     filepath=Path(filepath)
     filedir,filename=os.path.split(filepath)
-    
-    if filedir !="":
+    #creating filedir,filename
+    if filedir !="": # file dir is not empty
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"creating directory: {filedir} for the file: {filename}")
         
@@ -46,4 +46,4 @@ for filepath in list_of_files:
             logging.info(f"creating empty file: {filepath}")
     else:
         logging.info(f"{filepath} already exists")
-        
+    # it will created all folders like src, tests,github,etc   
